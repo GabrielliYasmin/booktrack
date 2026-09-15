4790329b3f89ec69efbca1bbe30086e232719b60
GabrielliYasmin <gabrielliyasmin28@gmail.com>
2026-09-15 16:53:39 -0300

    feat: adiciona menu e cadastro de livros

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Olá, BookTrack!")
+meus_livros = []
+
+while True:
+    print("\n--- MENU: ---")
+    print("1 - Adicionar livro")
+    print("2 - Listar livros")
+    print("3 - Atualizar leitura")
+    print("4 - Avaliar livro")
+    print("5 - Estatísticas")
+    print("0 - Sair")
+
+    opcao = input("Escolha a opção: ")
+
+    if opcao == "1":
+        livro = input("Digite o nome do livro: ")
+        meus_livros.append(livro)
+        print(f"✔️ Livro '{livro}' adicionado com sucesso!")
+
+    if opcao == "2":
+        for livro in meus_livros:
+            print(livro)
+
+    if opcao== "0":
+        print("Até logo, boa leitura! ")
+        break
